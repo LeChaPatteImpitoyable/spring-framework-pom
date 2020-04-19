ea20c105deb3629e8c358176b5b2bfb3f7730937
yingsy <yingshengyu@outlook.com>
2020-04-19 11:56:04 +0800

    init commit

--- spring-webmvc/src/test/java/com/ying/Test.java
+++ spring-webmvc/src/test/java/com/ying/Test.java
@@ -2,7 +2,6 @@ package com.ying;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.mock.web.MockServletConfig;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -25,13 +24,13 @@ public class Test {
 	 */
 	@org.junit.Test
 	public void test1() throws ServletException {
-		MockServletConfig config = new MockServletConfig("MyApp");
-		config.addInitParameter("contextConfigLocation","classpath*:applicationContext.xml");
-		DispatcherServlet dispatcherServlet = new DispatcherServlet();
-		dispatcherServlet.init(config);
-		WebApplicationContext webApplicationContext = dispatcherServlet.getWebApplicationContext();
-		GetBeanTest getBeanTest = (GetBeanTest) webApplicationContext.getBean("getBeanTest");
-		getBeanTest.showMe();
+//		MockServletConfig config = new MockServletConfig("MyApp");
+//		config.addInitParameter("contextConfigLocation","classpath*:applicationContext.xml");
+//		DispatcherServlet dispatcherServlet = new DispatcherServlet();
+//		dispatcherServlet.init(config);
+//		WebApplicationContext webApplicationContext = dispatcherServlet.getWebApplicationContext();
+//		GetBeanTest getBeanTest = (GetBeanTest) webApplicationContext.getBean("getBeanTest");
+//		getBeanTest.showMe();
 	}
 
 	/**
