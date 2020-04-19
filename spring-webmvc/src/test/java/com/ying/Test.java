64044e495e6c836c31fb6ad36941f1cc72523a48
yingsy <yingshengyu@outlook.com>
2020-04-19 09:16:58 +0800

    init commit

--- spring-webmvc/src/test/java/com/ying/Test.java
+++ spring-webmvc/src/test/java/com/ying/Test.java
@@ -4,9 +4,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.mock.web.MockServletConfig;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 
 public class Test {
@@ -17,6 +17,13 @@ public class Test {
 		System.out.printf(con.getApplicationName());
 	}
 
+	/**
+	 * servlet入口调用
+	 * org.springframework.web.servlet.FrameworkServlet#DEFAULT_CONTEXT_CLASS
+	 * 上面这个配置配置了默认的ApplicationContext为XmlWebApplicationContext
+	 * @throws ServletException
+	 */
+	@org.junit.Test
 	public void test1() throws ServletException {
 		MockServletConfig config = new MockServletConfig("MyApp");
 		config.addInitParameter("contextConfigLocation","classpath*:applicationContext.xml");
@@ -25,5 +32,15 @@ public class Test {
 		WebApplicationContext webApplicationContext = dispatcherServlet.getWebApplicationContext();
 		GetBeanTest getBeanTest = (GetBeanTest) webApplicationContext.getBean("getBeanTest");
 		getBeanTest.showMe();
+	}
+
+	/**
+	 * spring配置文件解析并加载bean
+	 * 使用XmlWebApplicationContext解析
+	 */
+	@org.junit.Test
+	public void test2(){
+		XmlWebApplicationContext xmlWebApplicationContext = new XmlWebApplicationContext();
+
 	}
 }
